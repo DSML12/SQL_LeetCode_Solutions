fca55d34227838b2a8542d8cf48197715bf69ee9
DSML12 <141859799+DSML12@users.noreply.github.com>
2026-09-20 09:55:46 -0500

    Added solution to 3554. Find Category Recommendation Pairs

---
+++ c_3554.sql
@@ -0,0 +1,31 @@
+--------------------------------------------------------------------------------
+-- 3554. Find Category Recommendation Pairs
+-- https://leetcode.com/problems/find-category-recommendation-pairs/description/
+--------------------------------------------------------------------------------
+
+With users_both As(
+    Select i.product_id, 
+      p.user_id, 
+      i.category
+    From ProductPurchases p
+    Inner Join ProductInfo i
+    on p.product_id = i.product_id
+), 
+  
+temp As (
+    Select u.category as category_1, 
+      u_2.category as category_2, 
+      u.user_id
+    From users_both u
+    Inner Join users_both u_2
+    On u.user_id = u_2.user_id 
+      AND u.category <> u_2.category
+      And u.category < u_2.category)
+
+Select category_1 as category1, 
+  category_2 as category2, 
+    Count(distinct user_id) as customer_count
+From temp
+Group by category_1,category_2
+Having Count(distinct user_id) >= 3
+Order by customer_count DESC, category1 ASC, category2 ASC;
