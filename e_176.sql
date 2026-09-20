2d972da82b1c23e111c0b4dad46c698f7deca2db
DSML12 <141859799+DSML12@users.noreply.github.com>
2026-09-20 10:02:31 -0500

    Added solution to 176. Second Highest Salary

---
+++ e_176.sql
@@ -0,0 +1,10 @@
+-------------------------------------------------------------------
+-- 176. Second Highest Salary
+-- https://leetcode.com/problems/second-highest-salary/description/
+-------------------------------------------------------------------
+
+Select IfnUll(max(salary),null) as SecondHighestSalary
+From
+    (Select salary, Dense_Rank() Over(Order by salary DESC) as rnk
+    From Employee) sub  
+where rnk=2
