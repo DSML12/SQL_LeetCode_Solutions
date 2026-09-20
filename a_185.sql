e009675711dbec572be2afd701e1d68d61931eac
DSML12 <141859799+DSML12@users.noreply.github.com>
2026-09-20 09:36:16 -0500

    Added link to 185. Department Top Three Salaries

--- a_185.sql
+++ a_185.sql
@@ -1,19 +1,27 @@
 --------------------------------------
 -- 185. Department Top Three Salaries
+-- https://leetcode.com/problems/department-top-three-salaries/
 --------------------------------------
 
 With ranks As (
-    Select name, salary, departmentId,
+    Select name, 
+        salary, 
+        departmentId,
         Dense_Rank() Over(Partition by departmentId Order by salary DESC) as rnk
     From Employee),
 
 top_3_ranks As (
-    Select name, salary, departmentId, rnk
+    Select name, 
+        salary, 
+        departmentId, 
+        rnk
     From ranks
     Where rnk < 4
 )
 
-Select d.name as Department, t.name as Employee, t.salary
+Select d.name as Department, 
+    t.name as Employee, 
+    t.salary
 From top_3_ranks t 
 Inner Join Department d
-on t.departmentId = d.id
+on t.departmentId = d.id;
