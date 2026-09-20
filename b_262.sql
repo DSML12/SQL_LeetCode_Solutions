92e0ec273ce60ec55f76c89b1c9510d379f02cbf
DSML12 <141859799+DSML12@users.noreply.github.com>
2026-09-20 10:07:11 -0500

    Updated solution to 262.  Trips and Users

--- b_262.sql
+++ b_262.sql
@@ -9,6 +9,7 @@ WITH not_banned_users AS (
     WHERE banned = 'No' 
       AND role IN ('client', 'driver')
 ),
+    
 filtered_dates AS (
     SELECT *
     FROM Trips
@@ -16,6 +17,7 @@ filtered_dates AS (
       AND driver_id IN (SELECT users_id FROM not_banned_users)
       AND request_at BETWEEN '2013-10-01' AND '2013-10-03'
 )
+    
 SELECT 
     request_at as Day,
     ROUND(1 - AVG(CASE WHEN status = 'completed' THEN 1 ELSE 0 END), 2) AS 'Cancellation Rate'
