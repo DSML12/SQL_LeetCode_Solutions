8e139a774256f13532300ab08c438246d1fb1973
DSML12 <141859799+DSML12@users.noreply.github.com>
2026-09-20 10:06:04 -0500

    Updated solution to 3554. Find Category Recommendation Pairs

--- c_3554.sql
+++ c_3554.sql
@@ -1,7 +1,7 @@
---------------------------------------------------------------------------------
+-------------------------------------------------------------------------------
 -- 3554. Find Category Recommendation Pairs
--- https://leetcode.com/problems/find-category-recommendation-pairs/description/
---------------------------------------------------------------------------------
+-- https://leetcode.com/problems/find-category-recommendation-pairs/description
+-------------------------------------------------------------------------------
 
 With users_both As(
     Select i.product_id, 
