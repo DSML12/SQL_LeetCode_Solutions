98a0587a3ece2f8520845f7c3ccc964bd35ebf3f
DSML12 <141859799+DSML12@users.noreply.github.com>
2026-09-20 10:04:18 -0500

    Updated solution to 185. Department Top Three Salaries

--- a_185.sql
+++ a_185.sql
@@ -1,7 +1,7 @@
---------------------------------------
+--------------------------------------------------------------------------
 -- 185. Department Top Three Salaries
--- https://leetcode.com/problems/department-top-three-salaries/
---------------------------------------
+-- https://leetcode.com/problems/department-top-three-salaries/description
+--------------------------------------------------------------------------
 
 With ranks As (
     Select name, 
